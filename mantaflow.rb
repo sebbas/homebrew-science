eb3bf44dbdd38ee560c550755bf560ff7b2ba5ae
Sebastián Barschkis <sebbas@sebbas.org>
2017-02-14 00:21:20 +0100

    mantaflow 0.9 (new formula)
    mantaflow is an open-source framework targeted at fluid simulation research in computer graphics.

---
+++ mantaflow.rb
@@ -0,0 +1,40 @@
+class Mantaflow < Formula
+  desc "Open computer graphics fluid simulation framework"
+  homepage "http://mantaflow.com/"
+  url "http://mantaflow.com/download/manta-src-0.9.tar"
+  sha256 "f55298f10650b35454f3134d2d0e265b19c24c3c68991e4bbedf5a460be67077"
+
+  option "with-openmp", "Build with OpenMP support"
+  option "with-qt5", "Build the QT GUI version"
+  option "with-scenes", "Install python sample scene files"
+
+  depends_on "cmake" => :build
+  depends_on "qt5" => :optional
+  depends_on :python => :build
+
+  needs :openmp if build.with? "openmp"
+
+  def install
+    args = std_cmake_args
+
+    args << "-DOPENMP=ON" if build.with? "openmp"
+    args << "-DGUI=ON" if build.with? "qt5"
+
+    system "cmake", ".", *args
+    system "make"
+
+    bin.install "manta"
+
+    # Symlink 'mantaflow' to avoid confusion when getting started with mantaflow
+    # Actual command is 'manta'
+    bin.install_symlink "manta" => "mantaflow"
+
+    # Copy the python sample scene files
+    pkgshare.install Dir["scenes/*"] if build.with? "scenes"
+  end
+
+  test do
+    touch "foo.py"
+    assert_match "Script finished.", shell_output("#{bin}/manta foo.py")
+  end
+end
